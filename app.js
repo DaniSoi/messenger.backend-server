7124bf5568d7e99d1ef4ac2f657d8484065f0210
daniel.soifer <danisoifer@gmail.com>
2020-08-06 00:07:41 +0300

    added path prefix to api router

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(routes.apiRouter);
+app.use(PATHS_PREFIX, routes.apiRouter);
 
 app.use('/healthcheck', healthCheckMiddleware());
 
